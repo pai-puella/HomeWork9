542614b2420120bf3940a3bad84963097edbc052
pai-puella <cat.dyshkant@yandex.ru>
2023-10-04 23:33:54 +0300

    Added task 1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
 public class Main {
+    public static void main(String[] args) {
+        task1();
+
+    }
+
+    // Объявить метод «сгенерироватьМассив»
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int j : arr) {
+            sum += j;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+    }
 }
